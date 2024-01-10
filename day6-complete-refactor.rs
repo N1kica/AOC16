9a15961f056a0b26f5234e878ea67c569630bd33
N1kica <68340084+N1kica@users.noreply.github.com>
2024-01-10 02:43:12 +0100

    Create day6-complete-refactor.rs

---
+++ day6-complete-refactor.rs
@@ -0,0 +1,65 @@
+use std::{fs, collections::BTreeMap};
+
+fn main() {
+    let contents = aoc::chars_per_line("./data/day6.txt", |c| Some(c));
+
+    let message = (0..contents[0].len()).map(|i| contents
+        .iter()
+        .map(|row| row[i])
+        .count_by_group()
+    ).fold(Message::new(), |acc, group| {
+        let max = group
+            .iter()
+            .max_by_key(|&(_, count)| count)
+            .map(|(ch, _)| ch)
+            .unwrap();
+
+        let min = group
+            .iter()
+            .min_by_key(|&(_, count)| count)
+            .map(|(ch, _)| ch)
+            .unwrap();
+
+        Message {
+            max: format!("{}{}", acc.max, max),
+            min: format!("{}{}", acc.min, min),
+        }
+    });
+
+    println!("Part 1: {}", message.max);
+    println!("Part 2: {}", message.min);
+
+}
+
+struct Message {
+    max: String,
+    min: String,
+}
+
+impl Message {
+    fn new() -> Self {
+        Message {
+            max: String::new(),
+            min: String::new(),
+        }
+    }
+}
+
+trait CountByGroup {
+    fn count_by_group(self) -> BTreeMap<char, usize>; // -> std::collections::btree_map::IntoIter<char, usize>
+}
+
+impl<I> CountByGroup for I
+where
+    I: Iterator<Item = char>,
+{
+    fn count_by_group(self) -> BTreeMap<char, usize> {
+        let mut counts = BTreeMap::new();
+
+        for c in self {
+            *counts.entry(c).or_insert(0) += 1;
+        }
+
+        counts
+    }
+}
